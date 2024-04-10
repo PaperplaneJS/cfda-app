cac916a80e324af76de24f61b23bf8dc5abba3d8
jia-niang <1@paperplane.cc>
2024-04-11 03:20:10 +0800

    chore(cdn): 新增上传 CDN 步骤

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,28 @@
+const S3Plugin = require('webpack-s3-plugin')
+
 module.exports = {
-  publicPath: '/',
+  publicPath: 'https://cdn.paperplane.cc/paperplane-cfda/',
   configureWebpack: {
     externals: {
       BMap: 'BMap',
     },
+  },
+  chainWebpack(config) {
+    config.plugin('S3Plugin').use(S3Plugin, [
+      {
+        exclude: /.*\.html$/,
+        basePath: 'paperplane-cfda',
+        s3Options: {
+          accessKeyId: process.env.COS_SECRET_ID,
+          secretAccessKey: process.env.COS_SECRET_KEY,
+          region: 'ap-hongkong',
+          endpoint: 'https://cos.ap-hongkong.myqcloud.com',
+          apiVersion: '2006-03-01',
+        },
+        s3UploadOptions: {
+          Bucket: 'paperplane-cdn-1253277322',
+        },
+      },
+    ])
   },
 }
